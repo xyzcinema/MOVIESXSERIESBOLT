9a16287138308d868c888bed94512e4e91500acb
xyzcinema <xyzcinema4@gmail.com>
2025-11-27 09:03:36 +0545

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -13,7 +13,7 @@ from handlers.helpers import str_to_b64
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"**Fɪʟᴇ/Vɪᴅᴇᴏ Wɪʟʟ Bᴇ Dᴇʟᴇᴛᴇᴅ Iɴ 30 Mɪɴꜱ. Tᴏ Sᴀᴠᴇ Tʜᴇᴍ Pʟᴇᴀꜱᴇ Fᴏʀᴡᴀʀᴅ Hᴇʀᴇ @File2LinkConverter_Bot**",
+            f"**Fɪʟᴇ/Vɪᴅᴇᴏ Wɪʟʟ Bᴇ Dᴇʟᴇᴛᴇᴅ Iɴ 60 Mɪɴꜱ.**",
             disable_web_page_preview=True,
             quote=True
         )
@@ -37,7 +37,7 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
-    asyncio.create_task(delete_after_delay(sent_message, 1800))
+    asyncio.create_task(delete_after_delay(sent_message, 3600))
 
 async def delete_after_delay(message, delay):
     await asyncio.sleep(delay)
